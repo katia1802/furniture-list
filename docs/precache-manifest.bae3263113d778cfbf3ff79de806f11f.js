e8366897d868b51bfcc3aac3f724b0cee22197c1
katia1802 <katia.rojas.sandoval@gmail.com>
2019-04-10 20:43:57 +0200

    gh pages

---
+++ docs/precache-manifest.bae3263113d778cfbf3ff79de806f11f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b65d2d40b3e6ed6cd909308752efcc01",
+    "url": "./static/media/apartment-architecture-artistic-1838065.b65d2d40.jpg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "2376c2c41b3fe11ee715",
+    "url": "./static/js/main.f82db87e.chunk.js"
+  },
+  {
+    "revision": "36c0bd0f8d97f9f3b068",
+    "url": "./static/js/2.e83f4017.chunk.js"
+  },
+  {
+    "revision": "2376c2c41b3fe11ee715",
+    "url": "./static/css/main.e84cd31f.chunk.css"
+  },
+  {
+    "revision": "5c9ecfa754c82068583034f5bf271fcc",
+    "url": "./index.html"
+  }
+];
