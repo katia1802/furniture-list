2b2b8d184747ac71b519284a2447f7f3eb27394b
katia1802 <katia.rojas.sandoval@gmail.com>
2019-04-17 13:49:21 +0200

    Sematic HTML correction

--- src/components/FurnitureList/index.js
+++ src/components/FurnitureList/index.js
@@ -5,36 +5,34 @@ import { Link } from "react-router-dom";
 
 class FurnitureList extends Component {
   render() {
-    const {furnitureResults}  = this.props;
+    const { furnitureResults } = this.props;
 
     if (furnitureResults.length === 0) {
-      return <div>No data found</div>
+      return <div>No data found</div>;
     } else {
       return (
-        <div className="furniture-list-container">
-          {furnitureResults.map((furniture) => {
+        <ul className="furniture-list" >
+          {furnitureResults.map(furniture => {
             // console.log ('furniture', furniture)
             return (
-              <ul className="furniture-list" key={furniture.id}>
-                <li className="furniture-item glowButton" >
-                  <Link
-                    className="furniture-link"
-                    to={`/furniture/${furniture.id}`}
-                  >
-                    <Furniture
-                      id={furniture.id}
-                      image={furniture.image}
-                      name={furniture.name}
-                      brand={furniture.brand}
-                      price={furniture.price}
-                      priceSale={furniture.priceSale}
-                    />
-                  </Link>
-                </li>
-              </ul>
+              <li className="furniture-item glowButton" key={furniture.id}>
+                <Link
+                  className="furniture-link"
+                  to={`/furniture/${furniture.id}`}
+                >
+                  <Furniture
+                    id={furniture.id}
+                    image={furniture.image}
+                    name={furniture.name}
+                    brand={furniture.brand}
+                    price={furniture.price}
+                    priceSale={furniture.priceSale}
+                  />
+                </Link>
+              </li>
             );
           })}
-        </div>
+        </ul>
       );
     }
   }
